ffc8d88ed1bc94e2a188a068901392280711d2fe
Simon Roberts <simon@dancingcloudservices.com>
2020-05-06 10:00:29 -0600

    Final

--- src/main/java/readmodifywrite/Increment.java
+++ src/main/java/readmodifywrite/Increment.java
@@ -3,6 +3,7 @@ package readmodifywrite;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.LongAdder;
 
 class SyncLong {
   // volatile addresses "visibility" NOT transactions
@@ -25,9 +26,11 @@ public class Increment {
 //    SyncLong theValue2 = new SyncLong();
     AtomicLong theValue = new AtomicLong();
     AtomicLong theValue2 = new AtomicLong();
+//    LongAdder theValue = new LongAdder();
+//    LongAdder theValue2 = new LongAdder();
 
     Runnable r = () -> {
-      for (int i = 0; i < 10_000_000; i++) {
+      for (int i = 0; i < 200_000; i++) {
         theValue.incrementAndGet();
         theValue2.incrementAndGet();
       }
@@ -35,7 +38,7 @@ public class Increment {
 
     long start = System.nanoTime();
     List<Thread> threads = new ArrayList<>();
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 1000; i++) {
       Thread t = new Thread(r);
       threads.add(t);
       t.start();
